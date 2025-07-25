fc58c589c06e7efad24948533c27f302a6a7f427
Angela <angdkay@gmail.com>
2025-07-25 17:50:11 -0600

    Access the image from image uploader
    Used FileReader which reads the image
    for us and turns it into a base64string.
    - reader = new FileReader()
    - reader.onload()
    - reader.result = function() that readasdaturl
      will run
    - reader.readAsDataUrl(image)

--- js/library.js
+++ js/library.js
@@ -1,8 +1,12 @@
-function Book(title, author, pages, isRead) {
+const readBooks = []
+const unreadBooks = []
+
+function Book(bookCover, title, author, pages, isRead) {
   if (!new.target) {
     throw Error("Must use 'new' operator");
   }
 
+  this.bookCover = bookCover;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -14,27 +18,43 @@ function Book(title, author, pages, isRead) {
 }
 
 form = document.querySelector("#book-form");
-console.log(form);
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const imageUpload = document.querySelector("#imageUpload").files[0];
 
+  console.log("File: ", imageUpload);
+
+  let bookCover = "";
   if (imageUpload) {
     const reader = new FileReader();
 
     reader.onload = function () {
       const imagePreview = document.createElement("img");
-      // use reader.result as the src image
-    }
-  }
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const totalPage = document.querySelector("#totalPages").value;
-  const pagesRead = document.querySelector("#pagesRead").value;
-  const hasRead = document.querySelector("#checkbox").checked;
+      imagePreview.src = reader.result;
 
-  console.log(`${title} by ${author} with total pages of ${totalPage} and pages read of ${pagesRead} added to library, also read = ${hasRead}`);
-  form.reset();
+      // use reader.result as the src image
+      document.body.appendChild(imagePreview);
+      bookCover = reader.result;
+
+      const title = document.querySelector("#title").value;
+      const author = document.querySelector("#author").value;
+      const totalPage = document.querySelector("#totalPages").value;
+      const pagesRead = document.querySelector("#pagesRead").value;
+      const hasRead = document.querySelector("#checkbox").checked;
+
+      const book = new Book(bookCover, title, author, totalPage, pagesRead, hasRead);
+
+      hasRead ? readBooks.push(book): unreadBooks.push(book);
+
+      console.log(`Confirmed book added to list in: ${JSON.stringify(readBooks)} or ${JSON.stringify(unreadBooks)}`);
+
+      console.log(`${title} by ${author} with total pages of ${totalPage} and pages read of ${pagesRead} added to library, also read = ${hasRead}`);
+      form.reset();
+    };
+
+    reader.readAsDataURL(imageUpload);
+  }
 });
+
