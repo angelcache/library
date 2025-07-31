e883761cf293f70c8854744ece113903538ca818
Angela <angdkay@gmail.com>
2025-07-30 22:44:47 -0600

    Add basic adding functionality

--- js/library.js
+++ js/library.js
@@ -1,7 +1,8 @@
-const readBooks = []
-const unreadBooks = []
+let readBooks = []
+let unreadBooks = []
+loadBooks();
 
-function Book(bookCover, title, author, pages, isRead) {
+function Book(bookCover, title, author, totalPage, pagesRead, isRead) {
   if (!new.target) {
     throw Error("Must use 'new' operator");
   }
@@ -9,7 +10,8 @@ function Book(bookCover, title, author, pages, isRead) {
   this.bookCover = bookCover;
   this.title = title;
   this.author = author;
-  this.pages = pages;
+  this.totalPage = totalPage;
+  this.pagesRead = pagesRead;
   this.isRead = isRead;
 
   function info() {
@@ -17,25 +19,18 @@ function Book(bookCover, title, author, pages, isRead) {
   }
 }
 
-form = document.querySelector("#book-form");
+const form = document.querySelector("#book-form");
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const imageUpload = document.querySelector("#imageUpload").files[0];
 
-  console.log("File: ", imageUpload);
-
   let bookCover = "";
   if (imageUpload) {
     const reader = new FileReader();
 
     reader.onload = function () {
-      const imagePreview = document.createElement("img");
-      imagePreview.src = reader.result;
-
-      // use reader.result as the src image
-      document.body.appendChild(imagePreview);
       bookCover = reader.result;
 
       const title = document.querySelector("#title").value;
@@ -44,17 +39,52 @@ form.addEventListener('submit', function(event) {
       const pagesRead = document.querySelector("#pagesRead").value;
       const hasRead = document.querySelector("#checkbox").checked;
 
+      console.log(hasRead);
       const book = new Book(bookCover, title, author, totalPage, pagesRead, hasRead);
 
-      hasRead ? readBooks.push(book): unreadBooks.push(book);
+      if (hasRead) {
+        readBooks.push(book);
+      } else {
+        unreadBooks.push(book);
+      }
+      addNewBook(book, hasRead);
 
-      console.log(`Confirmed book added to list in: ${JSON.stringify(readBooks)} or ${JSON.stringify(unreadBooks)}`);
-
-      console.log(`${title} by ${author} with total pages of ${totalPage} and pages read of ${pagesRead} added to library, also read = ${hasRead}`);
+      saveBooks();
       form.reset();
     };
 
-    reader.readAsDataURL(imageUpload);
+    reader.readAsDataURL(imageUpload)
   }
 });
 
+function saveBooks() {
+  localStorage.setItem("readBooks", JSON.stringify(readBooks));
+  localStorage.setItem("unreadBooks", JSON.stringify(unreadBooks));
+}
+
+function addNewBook(book, hasRead) {
+
+  console.log(hasRead);
+  const bookShelf = hasRead? document.querySelector(".read-books"): document.querySelector(".unread-books");
+
+  const newBook = document.createElement("img");
+
+  newBook.classList.add("book");
+  newBook.src = book.bookCover;
+  console.log(book.bookCover);
+  newBook.width = 130;
+  newBook.height = 190;
+
+  bookShelf.appendChild(newBook);
+}
+
+function loadBooks() {
+  readBooks = JSON.parse(localStorage.getItem("readBooks")) || [];
+  unreadBooks = JSON.parse(localStorage.getItem("unreadBooks")) || [];
+
+  // Re-render saved books when the page loads
+  readBooks.forEach(book => addNewBook(book, true));
+  unreadBooks.forEach(book => addNewBook(book, false));
+
+  console.log(unreadBooks);
+}
